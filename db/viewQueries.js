29844f74866600c9d8a396b026e9771bfca57769
Chris Waje <chriswaje324@gmail.com>
2023-11-24 16:05:29 -0800

    ADDED: inquirer prompt and view db queries

--- db/viewQueries.js
+++ db/viewQueries.js
@@ -13,7 +13,7 @@ const viewRoles = function (db) {
 // VIEW ALL EMPLOYEES
 const viewEmployees = function (db) {
     db.query(
-        'SELECT employees.id, employees.first_name, employees.last_name, roles.salary AS salary, roles.title AS title, departments.name AS department, employees.manager_id AS manager FROM roles JOIN employees ON employees.role_id = roles.id JOIN departments ON roles.department_id = departments.id ORDER BY employees.id'
+        'SELECT employees.id, employees.first_name, employees.last_name, roles.salary AS salary, roles.title AS title, departments.name AS department, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM roles JOIN employees manager ON manager.id = employees.manager_id JOIN employees ON employees.role_id = roles.id JOIN departments ON roles.department_id = departments.id'
         );
     // insert function to return to home directory
 };
